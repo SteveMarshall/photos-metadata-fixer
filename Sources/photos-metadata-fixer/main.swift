7d55b6a85940dfe81f37c97d37712c3c3fe94f0b
Steve Marshall <steve@stevemarshall.com>
2017-06-07 21:08:41 +0100

    Require an API key and instantiate FlickrAPI
    To do anything with the flickr API, we need an API key, so fail without
    that set as an environment variable. We could probably accept the API
    key as a command line parameter, but that then forces it to end up in
    the user's command history. This way, they could export it (or
    something) if they were particularly paranoid. Also this is easy(ish).
    
    To make printing to `FileHandle.standardError` easier, we're also
    extending `FileHandle` to conform to `TextOutputStream` so it can be
    used with `print(_:,to:)`. I can't work out how to test this easily so,
    for now, I'm not going to bother. I tried writing to tempfiles and
    reading them back and so on, but that didn't seem to work.

--- Sources/photos-metadata-fixer/main.swift
+++ Sources/photos-metadata-fixer/main.swift
@@ -1,6 +1,25 @@
 import PhotosMetadataFixerFramework
 import ScriptingBridge
 
+guard let flickrAPIKey = ProcessInfo.processInfo.environment[
+    "FLICKR_API_KEY"
+] else {
+    var standardError = FileHandle.standardError
+    print(
+        "⚠️  No FLICKR_API_KEY environment variable.",
+        "Without that, we can't access the flickr API.",
+        "\n- To get an API key, visit",
+        "https://www.flickr.com/services/apps/create/apply/,",
+        "apply for a non-commercial key, complete the form, and copy the key.",
+        "\n - To use the key with the app, prepend",
+        "FLICKR_API_KEY=<your key here> to the command you just executed.\n",
+        to: &standardError
+    )
+    exit(-1)
+}
+
+let api = FlickrAPI(withAPIKey: flickrAPIKey)
+
 if let photos: PhotosApplication = SBApplication(
     bundleIdentifier: "com.apple.Photos"
 ) {
